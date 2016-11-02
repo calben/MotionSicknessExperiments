c7c28bb7ac803c1667ed7ddd1e915e1bb2fadf21
Calem Bendell <calem.j.bendell@gmail.com>
2016-11-02 11:54:26 -0400

    add initial source code

---
+++ Source/MotionSickness/Player/SickeningPawn.cpp
@@ -0,0 +1,170 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MotionSickness.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "SickeningPawn.h"
+
+
+// Sets default values
+ASickeningPawn::ASickeningPawn()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MESH"));
+	RootComponent = Mesh;
+	Mesh->SetVisibility(false);
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("CAMERA"));
+	Camera->SetupAttachment(Mesh);
+	Camera->bLockToHmd = false;
+
+	Window = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("WINDOW"));
+	const ConstructorHelpers::FObjectFinder<UStaticMesh> WindowObj(TEXT("/Game/StarterContent/Architecture/Wall_Window_400x300"));
+	if (WindowObj.Object)
+		Window->SetStaticMesh(WindowObj.Object);
+
+	Window->SetRelativeLocation(FVector(90.f, 200.f, -55.f));
+	Window->SetRelativeRotation(FRotator(0.f, 270.f, 0.f));
+	Window->SetRelativeScale3D(FVector(1.f, 1.f, 0.4f));
+	Window->SetupAttachment(Camera);
+
+	SickeningSpeed = 5.f;
+}
+
+// Called when the game starts or when spawned
+void ASickeningPawn::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+// Called every frame
+void ASickeningPawn::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+	if (bIsSickening)
+	{
+		if (SickeningTimer >= SickeningRotatorChangeSeconds)
+		{
+			CurrentSickeningDirection = UKismetMathLibrary::RandomUnitVector();
+			if (!bSickenX)
+			{
+				CurrentSickeningDirection.X = 0;
+			}
+			if (!bSickenY)
+			{
+				CurrentSickeningDirection.Y = 0;
+			}
+			if (!bSickenZ)
+			{
+				CurrentSickeningDirection.Z = 0;
+			}
+			if (CurrentSickeningDirection.Size() > 0)
+			{
+				CurrentSickeningDirection /= CurrentSickeningDirection.Size();
+			}
+			SickeningTimer = 0.f;
+		}
+		AddControllerPitchInput(CurrentSickeningDirection.X * SickeningSpeed * DeltaTime);
+		AddControllerYawInput(CurrentSickeningDirection.Y * SickeningSpeed * DeltaTime);
+		AddControllerRollInput(CurrentSickeningDirection.Z * SickeningSpeed * DeltaTime);
+		SetActorRotation(this->GetControlRotation());
+	}
+	SickeningTimer += DeltaTime;
+}
+
+// Called to bind functionality to input
+void ASickeningPawn::SetupPlayerInputComponent(class UInputComponent* InputComponent)
+{
+	Super::SetupPlayerInputComponent(InputComponent);
+
+	InputComponent->BindAction("ToggleSickening", IE_Pressed, this, &ASickeningPawn::ToggleSickening);
+	InputComponent->BindAction("ToggleBlinders", IE_Pressed, this, &ASickeningPawn::ToggleWindow);
+	InputComponent->BindAction("IncreaseSickenSpeed", IE_Pressed, this, &ASickeningPawn::IncreaseSickenSpeed);
+	InputComponent->BindAction("DecreaseSickenSpeed", IE_Pressed, this, &ASickeningPawn::DecreaseSickenSpeed);
+	InputComponent->BindAction("IncreaseTimerSpeed", IE_Pressed, this, &ASickeningPawn::IncreaseTimerSpeed);
+	InputComponent->BindAction("DecreaseTimerSpeed", IE_Pressed, this, &ASickeningPawn::DecreaseTimerSpeed);
+	InputComponent->BindAction("SetupTrial", IE_Pressed, this, &ASickeningPawn::SetupTrial);
+	InputComponent->BindAction("EnableX", IE_Pressed, this, &ASickeningPawn::EnableX);
+	InputComponent->BindAction("EnableY", IE_Pressed, this, &ASickeningPawn::EnableY);
+	InputComponent->BindAction("EnableZ", IE_Pressed, this, &ASickeningPawn::EnableZ);
+	InputComponent->BindAction("IncreaseFOV", IE_Pressed, this, &ASickeningPawn::IncreaseFOV);
+	InputComponent->BindAction("DecreaseFOV", IE_Pressed, this, &ASickeningPawn::DecreaseFOV);
+	InputComponent->BindAction("ResetCamera", IE_Pressed, this, &ASickeningPawn::ResetCamera);
+}
+
+void ASickeningPawn::ToggleSickening()
+{
+	bIsSickening = !bIsSickening;
+	SickeningTimer = 999.f;
+}
+
+void ASickeningPawn::ToggleWindow()
+{
+	Window->SetVisibility(!Window->bVisible);
+}
+
+void ASickeningPawn::IncreaseSickenSpeed()
+{
+	SickeningSpeed += 1.f;
+	SickeningTimer = 999.f;
+}
+
+void ASickeningPawn::DecreaseSickenSpeed()
+{
+	SickeningSpeed -= 1.f;
+	SickeningTimer = 999.f;
+}
+
+void ASickeningPawn::IncreaseTimerSpeed()
+{
+	SickeningRotatorChangeSeconds += 1.f;
+	SickeningTimer = 999.f;
+}
+
+void ASickeningPawn::DecreaseTimerSpeed()
+{
+	SickeningRotatorChangeSeconds -= 1.f;
+	SickeningTimer = 999.f;
+}
+
+void ASickeningPawn::EnableX()
+{
+	bSickenX = !bSickenX;
+	SickeningTimer = 999.f;
+}
+
+void ASickeningPawn::EnableY()
+{
+	bSickenY = !bSickenY;
+	SickeningTimer = 999.f;
+}
+
+void ASickeningPawn::EnableZ()
+{
+	bSickenZ = !bSickenZ;
+	SickeningTimer = 999.f;
+}
+
+void ASickeningPawn::IncreaseFOV()
+{
+	Camera->SetFieldOfView(Camera->FieldOfView + 5.0f);
+}
+
+void ASickeningPawn::DecreaseFOV()
+{
+	Camera->SetFieldOfView(Camera->FieldOfView - 5.0f);
+}
+
+void ASickeningPawn::SetupTrial()
+{
+	SickeningSpeed = SpeedMultiplierTestPool[(int32) FMath::RandRange(0, SpeedMultiplierTestPool.Num())];
+	SickeningRotatorChangeSeconds = DirectionChangeFrequency[(int32) FMath::RandRange(0, DirectionChangeFrequency.Num())];
+}
+
+void ASickeningPawn::ResetCamera()
+{
+	GetController()->SetControlRotation(FRotator::ZeroRotator);
+	SetActorRotation(GetControlRotation());
+}
---
+++ Source/MotionSickness/Player/SickeningPawn.h
@@ -0,0 +1,96 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Pawn.h"
+#include "SickeningPawn.generated.h"
+
+UCLASS()
+class MOTIONSICKNESS_API ASickeningPawn : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ASickeningPawn();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void Tick( float DeltaSeconds ) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* InputComponent) override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UStaticMeshComponent* Mesh;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UCameraComponent* Camera;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		UStaticMeshComponent* Window;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		bool bIsSickening;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		FVector CurrentSickeningDirection;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		float SickeningSpeed = 1.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		bool bSickenX = true;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		bool bSickenY = true;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		bool bSickenZ = true;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		float SickeningRotatorChangeSeconds = 10.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		float SickeningTimer = 0.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		bool bTrialMode;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		float TrialTimer = 5.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		TArray<int32> SpeedMultiplierTestPool = { 1 / 8, 1 / 4, 1 / 2, 1, 2, 4, 8, 16, 32 };
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		TArray<int32> DirectionChangeFrequency = { 1 / 32, 1 / 16, 1 / 8, 1 / 4, 1, 2, 4, 8, 16, 32 };
+
+	void ToggleSickening();
+
+	void ToggleWindow();
+
+	void IncreaseSickenSpeed();
+
+	void DecreaseSickenSpeed();
+
+	void IncreaseTimerSpeed();
+
+	void DecreaseTimerSpeed();
+
+	void EnableX();
+
+	void EnableY();
+
+	void EnableZ();
+
+	void IncreaseFOV();
+
+	void DecreaseFOV();
+
+	void SetupTrial();
+
+	void ResetCamera();
+};
